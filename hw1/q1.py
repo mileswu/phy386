2173d00c267d68f28af5b9af247eae3573e824bb
Miles Wu <miles.wu@wu-home.co.uk>
2013-04-05 16:43:01 -0500

    Fix normalization

--- hw1/q1.py
+++ hw1/q1.py
@@ -5,7 +5,10 @@ import math
 import matplotlib.pyplot as pyplot
 
 n = 1024
-deltat = 0.01
+#deltat = 0.01
+deltat = 1.0/n
+#n = 4096
+#deltat = 1.0
 amplitude = 1.0
 
 t = numpy.arange(n) * deltat
@@ -19,5 +22,8 @@ freq = numpy.arange(n/2 + 1) / deltat / n
 
 sinewave_fft_absrmsnormalized = numpy.abs(sinewave_fft) / n * math.sqrt(2.0)
 
-pyplot.plot(freq, numpy.abs(sinewave_fft_absrmsnormalized))
-pyplot.show()
+print sinewave_fft_absrmsnormalized
+print freq
+
+#pyplot.plot(freq, numpy.abs(sinewave_fft_absrmsnormalized))
+#pyplot.show()
