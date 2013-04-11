39009b2e5c504bb05e4fac8bd4c5fb9ac102e923
Miles Wu <miles.wu@wu-home.co.uk>
2013-04-10 21:16:56 -0500

    Finished q2

--- hw1/q2.py
+++ hw1/q2.py
@@ -8,31 +8,40 @@ n = 1024
 deltat = 1.0
 
 t = numpy.arange(n) * deltat
-whitenoise = numpy.random.normal(0.0, 1.0, n)
-sinewave = 0.4*numpy.sin(t * 2.0 * math.pi / n / deltat)
-
-pyplot.plot(t, whitenoise + sinewave)
-pyplot.show()
-
-whitenoise_fft = numpy.fft.rfft(whitenoise + sinewave)
+whitenoise = numpy.random.normal(0.0, 1.0, n/2+1) + 1j*numpy.random.normal(0.0, 1.0, n/2+1)
+whitenoise[0] = 0
 freq = numpy.arange(n/2 + 1) / deltat / n
 
-freqfilter = numpy.append(numpy.ones(freq.size/10), numpy.zeros(freq.size - freq.size/10))
-whitenoise_fft_filtered = whitenoise_fft * freqfilter
+tau = 5.0
+rcfilter = 1.0 / (1.0 + 1j*2.0*math.pi*freq*tau)
+whitenoise_filtered = whitenoise * rcfilter
+print "Integral of PS: %f" % numpy.sum(numpy.abs(whitenoise_filtered)**2/n/n)
 
-#Integral of power
-whitenoise_fft_filtered_power = numpy.abs(whitenoise_fft_filtered/n)**2 * 2.0
-print freq[1]
-print numpy.sum(whitenoise_fft_filtered_power)*freq[1]
-pyplot.plot(freq, whitenoise_fft_filtered_power)
+whitenoise_t = numpy.fft.irfft(whitenoise_filtered)
 
-whitenoise_filtered = numpy.fft.irfft(whitenoise_fft_filtered)
-autocorr = numpy.correlate(whitenoise_filtered, whitenoise_filtered, 'full')[n-1:]
-autocorr = autocorr / numpy.arange(n, 0, -1)
+autocorr2 = numpy.correlate(whitenoise_t, whitenoise_t, 'full')[n-1:]
+print "Integral of autocorrelation = %f" % numpy.sum(autocorr2/n)
 
-print numpy.sum(autocorr)*t[1]
+pyplot.figure(1, figsize=(10.0, 12.0))
 
+pyplot.subplot(411)
+pyplot.plot(freq, numpy.abs(whitenoise)**2/n/n)
+pyplot.xlabel("Frequency [Hz]")
+pyplot.ylabel("Power spectrum [arb^2]")
+#pyplot.subplots_adjust(hspace = 0.5)
 
-#pyplot.plot(t, whitenoise_filtered)
-pyplot.plot(t, autocorr)
-pyplot.show()
+pyplot.subplot(412)
+pyplot.plot(freq, numpy.abs(whitenoise_filtered)**2/n/n)
+pyplot.xlabel("Frequency [Hz]")
+pyplot.ylabel("Filtered Power spectrum [arb^2]")
+
+pyplot.subplot(413)
+pyplot.plot(t, whitenoise_t)
+pyplot.xlabel("Time [s]")
+pyplot.ylabel("Band-limited time stream [arb]")
+
+pyplot.subplot(414)
+pyplot.plot(t, autocorr2/n)
+pyplot.xlabel("Lag Time [s]")
+pyplot.ylabel("Autocorrelation [arb^2]")
+pyplot.savefig("q2.png")
